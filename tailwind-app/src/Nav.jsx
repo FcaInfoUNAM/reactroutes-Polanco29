556f39489f753922ea28ad6b05df3c6da3997650
Polanco29 <lopezpolancoalejandro6@gmail.com>
2025-05-28 19:40:50 -0600

    Propiedades de los componentes

--- tailwind-app/src/Nav.jsx
+++ tailwind-app/src/Nav.jsx
@@ -20,6 +20,13 @@ function Nav (){
     </button>
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+         {/* 
+    Aquí usamos el método .map() para recorrer el arreglo 'navigation'.
+    Por cada objeto 'e' dentro del arreglo, creamos un <li> que contiene un <a> con su respectivo título y enlace.
+    Esto nos permite generar dinámicamente el menú de navegación a partir de la estructura del arreglo.
+    Cada 'e' representa un elemento del menú, donde 'e.title' es el texto que verá el usuario y 'e.href' es la ruta.
+  */}
+        
         {navigation.map(e=>
         <li>
           <a key={e.title} href={e.href}  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">{e.title}</a>
